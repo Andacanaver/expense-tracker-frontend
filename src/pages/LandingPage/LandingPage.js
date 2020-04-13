f11c57d12a8c552f204706e53f514f3d7fd9a58a
Jacob Moe <andacanaver@Jacobs-MacBook-Pro.local>
2020-04-13 09:57:54 -0500

    fix landing page styling.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ export default class LandingPage extends Component {
     render() {
         return (
             <section className='landing-page'>
-                <div>
+                <div className='landing-about'>
                     <p>This is an expense tracker app to help keep track of expenses and hopefully help to see where a user can cut back their spending or just to track what they're spending on. Click <Link to='/login'>Here</Link> to login to a demo account or click <Link to='/register'>Here</Link> to register an account.</p>
                 </div>
                 <div className='landing-image'>
