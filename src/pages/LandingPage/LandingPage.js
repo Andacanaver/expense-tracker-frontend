c1d38b8d838b729240336e085c7f81a1d4fbd55d
Jacob Moe <andacanaver@Jacobs-MacBook-Pro.local>
2020-04-15 17:10:47 -0500

    add mobile view screenshot and tweak mobile view

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,7 +2,16 @@ import React, {Component} from 'react'
 import './LandingPage.css'
 import { Link } from 'react-router-dom'
 import LandingImage from '../../landing-image.png'
+import { BrowserView, MobileView, isMobile } from 'react-device-detect'
+import ExpenseMobile from '../../expense-mobile.png'
+
 export default class LandingPage extends Component {
+    renderContent = () => {
+        if (isMobile) {
+            return <img src={ExpenseMobile} alt='mobile screenshot of expenses page' />
+        }
+        return <img src={LandingImage} alt='screenshot of what the expense page looks like' />
+    }
     render() {
         return (
             <section className='landing-page'>
@@ -10,7 +19,9 @@ export default class LandingPage extends Component {
                     <p>This is an expense tracker app to help keep track of expenses and hopefully help to see where a user can cut back their spending or just to track what they're spending on. Click <Link to='/login'>Here</Link> to login to a demo account or click <Link to='/register'>Here</Link> to register an account.</p>
                 </div>
                 <div className='landing-image'>
-                    <img src={LandingImage} alt='screenshot of what the expense page looks like'/>
+                    <p>Below is a picture of what the expenses page looks like.</p>
+                    {this.renderContent()}
+                    
                 </div>
             </section>
         )
